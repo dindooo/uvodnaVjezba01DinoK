558a11fafe68accf012bdc15fdd56adb5ceac886
dindooo <145918377+dindooo@users.noreply.github.com>
2023-09-24 17:00:38 +0200

    Add files via upload

---
+++ uvodnaVjezba01DinoK.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+
+            a=100;
+            b=3;
+            x=a/b;
+            y=a%b;
+
+            Console.WriteLine(a + " dijeljeno sa " + b + " je jednako " + x + ", a ostatak iznosi " + y);
+            Console.ReadKey();
+        }
+    }
+}
